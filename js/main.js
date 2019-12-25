1461b6184bf00a7e2317bc5cecd90e1f5ebb0653
marijabiberdzic <marijabiberdzic@users.noreply.github.com>
2019-12-25 20:03:17 +0100

    promenjeno

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//index.html
+/* $('#navId a').click(e => {
+    e.preventDefault();
+    $(this).tab('show');
+}); */
+
+var datum= new Date();
+document.getElementById("dateTime").innerHTML= datum.toLocaleString();
+
+
+// contact.html
+
